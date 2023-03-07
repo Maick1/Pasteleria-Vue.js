2ec1e877d8abee302c5fc911d6cfed5c9f6d0bfd
Maick1 <86171053+Maick1@users.noreply.github.com>
2023-03-06 19:06:58 -0500

    octavo/medio
    modificacion de pinia

--- src/store/cart.js
+++ src/store/cart.js
@@ -1,16 +1,22 @@
-//Se importa la libreria Pinia
+// Se importa la libreria Pinia
 import { defineStore } from "pinia";
-//Importamos la Data donde estan los pasteles :3
+
+// Importamos la Data donde están los pasteles :3
 import products from "@/data";
 
-//Creamos nuesta Store con la api de pinia
+// Creamos nuestra Store con la api de Pinia
 export const useCartStore = defineStore("cart", {
+
+  // Definimos el estado inicial de la Store
   state: () => ({
-    //Variables donde se almacena lo añadido del carrito
-    cartContent: {},
-    theme: "light",
+    cartContent: {}, // Un objeto donde se almacenará la cantidad de cada producto que se ha agregado al carrito
+    theme: "light", // El tema inicial será "light"
   }),
+
+  // Definimos los getters, que son funciones que nos permiten obtener información de la Store
   getters: {
+
+    // Este getter devuelve un arreglo con la información detallada de cada producto que se ha agregado al carrito
     formattedCart: (state) => {
       return Object.keys(state.cartContent).map((productId) => {
         const product = products.find((p) => p.id === productId);
@@ -24,24 +30,32 @@ export const useCartStore = defineStore("cart", {
         };
       });
     },
+
+    // Este getter devuelve el total del valor de los productos en el carrito
     total: (state) => {
       return Object.keys(state.cartContent).reduce((acc, id) => {
         const product = products.find((p) => p.id === id);
         return product ? acc + product.price * state.cartContent[id].quantity : acc;
       }, 0);
     },
+
+    // Este getter devuelve el número total de productos en el carrito
     productsTotal: (state) => {
       return Object.keys(state.cartContent).reduce((acc, id) => {
         return acc + state.cartContent[id].quantity;
       }, 0);
     },
+
+    // Este getter devuelve el tema actual de la aplicación
     getTheme: (state) => {
       return state.theme;
     },
   },
-  //Acciones
+
+  // Definimos las acciones, que son funciones que nos permiten modificar el estado de la Store
   actions: {
-    //Accion de agregar pasteles al carro
+
+    // Esta acción permite agregar un producto al carrito
     add(productId) {
       if (!this.cartContent[productId]) {
         this.cartContent[productId] = {
@@ -51,15 +65,21 @@ export const useCartStore = defineStore("cart", {
       }
       this.cartContent[productId].quantity += 1;
     },
+
+    // Esta acción permite remover una unidad de un producto del carrito
     remove(productId) {
       this.cartContent[productId].quantity = this.cartContent[productId]?.quantity - 1 || 0;
       if (this.cartContent[productId].quantity === 0) {
         delete this.cartContent[productId];
       }
     },
+
+    // Esta acción permite remover completamente un producto del carrito
     removeProduct(productId) {
       delete this.cartContent[productId];
     },
+
+    // Esta acción permite cambiar el tema de la aplicación
     toggleTheme() {
       this.theme = this.theme === "light" ? "dark" : "light";
     },
